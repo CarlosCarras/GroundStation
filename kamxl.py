5b825de1dc7ab25d410a2e2941166a443bc6752a
Carlos Carrasquillo <c.carrasquillo@ufl.edu>
2021-06-06 13:11:53 -0500

    added basic AX.25 receive functionality using a new library

--- kamxl.py
+++ kamxl.py
@@ -17,27 +17,38 @@ from aioax25.frame import AX25UnnumberedInformationFrame
 
 class KAMXL_TNC():
     def __init__(self):
+        self.src = telecommands.DST_CALLSIGN
+        self.dst = telecommands.DST_CALLSIGN
+
         self.kissdev = SerialKISSDevice(
             device='/dev/ttyS0',
             baudrate=9600,
             log=logging.getLogger('./assets/D3_KISS.log'))
+
         self.ax25int = AX25Interface(
             kissport= self.kissdev[0],
             log=logging.getLogger('./assets/D3_AX25.log'))
-        self.kissdev.open()
 
+        self.ax25int.bind(
+            callback=self.read,
+            callsign=self.dst,
+            ssid=None)
+
+        self.kissdev.open()
 
     def print_inbound(self, x):
         print(x)                                    # prints whatever is passed in.
 
-    def read(self):
-        self.print_inbound("Receive not yet implemented.")
+    def read(self, interface, frame, **kwargs):
+        self.print_inbound(frame.payload)
+        return frame
 
     def write(self, text):
         payload = bytes(text, 'utf-8')
+
         frame = AX25UnnumberedInformationFrame(
-            destination=telecommands.DST_CALLSIGN,
-            source=telecommands.SRC_CALLSIGN,
+            destination=self.dst,
+            source=self.src,
             pid=0xf0,
             payload=payload)
 
